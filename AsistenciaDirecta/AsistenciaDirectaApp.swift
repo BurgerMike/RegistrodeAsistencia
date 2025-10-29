dc54d99d370d8e946f36d1165aa8a686fb80142f
BurgerMike <mikelizondojr@hotmail.com>
2025-10-29 15:27:13 -0600

    Initial Commit

---
+++ AsistenciaDirecta/AsistenciaDirectaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsistenciaDirectaApp.swift
+//  AsistenciaDirecta
+//
+//  Created by Miguel Carlos Elizondo Martinez on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AsistenciaDirectaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
